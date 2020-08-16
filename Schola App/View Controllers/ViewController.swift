93248e284e63e86ea77c5194d96589a1513b1ebb
Natalie Ramirez <60053094+natalieramirez@users.noreply.github.com>
2020-08-16 02:08:41 -0400

    final app

--- Schola App/View Controllers/ViewController.swift
+++ Schola App/View Controllers/ViewController.swift
@@ -29,6 +29,18 @@ class ViewController: UIViewController {
         Utilities.styleFilledButton(signUpButton)
         Utilities.styleHollowButton(loginButton)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "schola", ofType: "mp4")!))
+        
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = view.bounds
+        view.layer.addSublayer(layer)
+        
+        player.play()
+    }
 
 }
 
